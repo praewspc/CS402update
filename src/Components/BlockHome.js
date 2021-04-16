bb18bd5a97eb49cce6408624578720b5ecbccf90
praewspc <praewspc@gmail.com>
2021-04-17 03:20:22 +0700

    170464

--- src/Components/BlockHome.js
+++ src/Components/BlockHome.js
@@ -9,7 +9,7 @@ function BlockHome() {
         <div className="rightblockhome">
             <div className="blockwhitehome">
 
-                {/* <div classname="block top">
+                <div classname="block top">
                     <Row classname="blueblue">
                         <Col>
                             <div className="block1">
@@ -22,12 +22,12 @@ function BlockHome() {
 
 
                         </Col>
-                        <Col>
+                        {/* <Col>
                             <div className="block3">
                                 <FaSearch />
                                 <p classname="Phurit">Phurit Apichonlati</p>
                             </div>
-                        </Col>
+                        </Col> */}
                     </Row>
                 </div>
 
@@ -37,23 +37,36 @@ function BlockHome() {
                     <div class="a">
                         <div className="paragraph1">
                             <p className="cs">CS401 / CS402</p>
-                            <p className="cs1">วิชาบังคับก่อนศึกษา: </p>
+                            <p className="cs1">วิชาบังคับก่อนศึกษา: 
+                                <span className="cs1-1">สําหรับนักศึกษาตั้งแต่ชั้นปีที่ 4 ขึ้นไป และ สอบได้ระดับ S ในรายวิชา คพ.300 และเคยศึกษา คพ.301</span>
+                            </p>
+                            
                         </div>
 
                         <div className="paragraph2">
-                            <p className="cs2">คำอธิบายรายวิชา: </p>
+                            <p className="cs2">คำอธิบายรายวิชา: 
+                                <span className="cs2-1"> โครงงานการศึกษาจะกําหนดหรือได้รับอนุมัติจากภาควิชา ศึกษาหัวข้อทางวิชาการ โดยมีวัตถุประสงค์ให้นักศึกษาค้นคว้าทางวิชาการ เขียนรายงาน และเสนอต่อคณะ
+กรรมการ
+Senior Projects Will Be Assigned Or Approved By The Department. This 
+Course Prepares Students For Developing A Proposal For Doing Research, Writing Research Papers, And Presenting The Proposal To The Committee.</span>
+                            </p>
                         </div>
 
                         <div className="paragraph3">
-                            <p className="cs3">ผู้ประสานงาน: </p>
+                            <p className="cs3">ผู้ประสานงาน: 
+                                <span className="cs3-1">อาจารย์ นุชชากร งามเสาวรส (FB: Nutchakorn Ngam)</span>
+                                <span className="cs3-2">อาจารย์ ดร.วนิดา พฤทธิวิทยา (Wanidap@Cs.Tu.Ac.Th)</span>
+                                
+                            </p>
+                            
                         </div>
                     </div>
 
-                    <div class="b">
+                    {/* <div class="b">
 
 
-                    </div>
-                </div> */}
+                    </div> */}
+                </div>
             </div>
 
 
